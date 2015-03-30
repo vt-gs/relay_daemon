0c1464a0285f635cf05b859ee9f7d0e7150c5d60
Zachary James Leffke <zleffke@vt.edu>
2015-03-30 10:07:00 -0400

    working version of GUI.  relay callback working, relay value working, reset button working.

--- python/rr_control_gui.py
+++ python/rr_control_gui.py
@@ -4,50 +4,12 @@ import os
 import string
 import sys
 import time
-import curses
-import threading
 from optparse import OptionParser
 from binascii import *
 from remote_relay import *
 from rr_main_gui import *
-#from curses_display import *
+from data_thread_rr import *
 
-
-class Data_Server(threading.Thread):
-    def __init__ (self, options):
-        threading.Thread.__init__(self)
-        self._stop = threading.Event()
-        self.ip = options.ip
-        self.port = options.port
-        self.suspend = False
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP Socket
-        self.packet_list = []
-        self.rtt_tlm = rtt_tlm()
-
-    def run(self):
-        self.sock.bind((self.ip, self.port))
-        while (not self._stop.isSet()):
-            if self.suspend == False: 
-                data = self.sock.recv(1024)
-                if len(data) == 256:
-                    self.packet_list.append(data)
-                    self.Decode_Packet(data)
-                #print len(data)
-                #for i in range(len(data)):
-                #    print hex(ord(data[i]))
-        sys.exit()
-
-    def Decode_Packet(self, data):
-        self.gui.raw_textbox.insertPlainText(str(binascii.hexlify(data)) + '\n\n')
-
-    def set_gui_access(self, gui_handle):
-        self.gui = gui_handle
-
-    def stop(self):
-        self._stop.set()
-
-    def stopped(self):
-        return self._stop.isSet()
 
 if __name__ == '__main__':
 	
