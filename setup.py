2dd2b7149c78525131184b61c78f49e504e6efc2
Zach Leffke <zleffke@vt.edu>
2017-12-12 09:04:57 -0500

    going back to 'installable' plan.  adding setup.py and init.py files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+    Remote Relay Daemon.
+"""
+
+try:
+    import setuptools
+except ImportError:
+    print("=========================================================")
+    print(" Relay Daemon requires setuptools for installing         ")
+    print(" You can install setuptools using pip:                   ")
+    print("    $ pip install setuptools                             ")
+    print("=========================================================")
+    exit(1)
+
+
+from setuptools import setup
+import relay_daemon
+
+setup(
+    name    ='Relay Daemon',    	# This is the name of your PyPI-package.
+    version =__version__,
+    url     =__url__,
+    author  =__author__,
+    author_email = __email__,
+    #scripts=['relay_daemon']  # executable name  
+    entry_points={
+        "console_scripts": ["relay_daemon = relay_daemon.main:main"]
+    }
+)
