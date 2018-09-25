679dcb643b9007872e442843c0a04a9123829b6e
ksterne <kevintyler@vt.edu>
2018-09-25 13:41:08 -0400

    Massively debugging

--- relay_daemon/numato.py
+++ relay_daemon/numato.py
@@ -44,15 +44,17 @@ class Ethernet_Relay(threading.Thread):
             self.connect()
         while (not self._stop.isSet()):
             if self.connected:
+                print "connected!"
                 if (not self.tx_q.empty()): #Message for Relay Bank Received
                     msg = self.tx_q.get()
                     print '{:s} | {:s}'.format(self.name, msg)
                     if 'READ' in msg:
+                        print "Time to read!"
                         self.read_all_relays()
                         self.rx_q.put(self.state)
-                        
             else:
                 time.sleep(5)
+                print "Trying to reconnect..."
                 self.connect()
 
             time.sleep(0.01) #Needed to throttle CPU
@@ -63,19 +65,27 @@ class Ethernet_Relay(threading.Thread):
     def connect(self):
         try:
             self.logger.info('Attempting to telnet to relay bank: {:s}'.format(self.ip))
+            print "one"
             self.tn = telnetlib.Telnet(self.ip)
+            print "Object create"
             self.tn.read_until("User Name: ")
+            print "Read use name"
             self.tn.write(self.username + "\n")
-            #print 'entered login'
+            print 'entered login'
             if self.password:
+                print "waiting for password"
                 self.tn.read_until("Password: ")
+                print "Found password"
                 self.tn.write(self.password + "\n")
-            resp = self.tn.read_until('>>')
-            #print resp
+            print "waiting for prompt"
+            resp = self.tn.read_until('>')
+            print "Found prompt"
+            print resp
             self.logger.info('Succesful telnet to relay bank: {:s}'.format(self.ip))
             self.connected = True
             print 'Connected!'
-        except:
+        except Exception as e:
+            print str(e)
             self.logger.info('Failed to telnet to relay bank: {:s}'.format(self.ip))
             self.connected = False
 
